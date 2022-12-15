0891856189393c9fa0fc3bf41aa99f7e36074428
Anshika Tripathi <120476909+anshika410@users.noreply.github.com>
2022-12-15 19:56:30 +0530

    Update quiz.js
    docs: Updated the quiz questions.

--- quiz.js
+++ quiz.js
@@ -1,7 +1,7 @@
 var readlineSync=require("readline-sync");
 var userName=readlineSync.question("May I nkow your name? ");
 console.log("** Welcome "+userName+' **');
-console.log("Let's start the quiz...")
+console.log("Let's check how much you know me...")
 var score=0;
 function quiz(question, answer){
   var userAnswer=readlineSync.question(question)
@@ -19,9 +19,9 @@ function quiz(question, answer){
     console.log("--------------")
   }
 }
-quiz("Q-1 who is the prime minister of India? ", "Narendra Modi");
-quiz("Q-2 What is the capital of India? ", "New Delhi");
-quiz("Q-3 How many states are in India? ", "29");
-quiz("Q-4 What is the national anthem of India?", "Jan Gan Man");
-quiz("Q-5 What is the national song of India?", "Vande Mataram");
+quiz("Q-1 which is my favourite song? ", "Beh chala");
+quiz("Q-2 which is my favourite colour? ", "Black");
+quiz("Q-3 What i like the most among Coffee and Tee? ", "Tea");
+quiz("Q-4 What I prefer to travel among Flight and Train?", "Train");
+quiz("Q-5 Which is my favourite movie?", "Uri");
 console.log("yay!!! Your total score is : "+ score);
